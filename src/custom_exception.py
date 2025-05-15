c8add1467b403ca4e53ad60da5d9fd346bd1a917
Vivek Raj <vivekr@asiapac.altair.com>
2025-05-15 18:58:19 +0530

    commit

--- src/custom_exception.py
+++ src/custom_exception.py
@@ -1,3 +1,9 @@
+# Think of it like a special alarm system that not only tells you something went wrong, but also tells you:
+# Where it went wrong (which file)
+# Exactly which line in the file
+# And optionally, the error message itself
+
+
 import traceback  ## for tracking the error
 import sys
 
@@ -8,7 +14,7 @@ class CustomException(Exception):
 
     @staticmethod
     def get_detailed_error_message(error_message,error_detail: sys) -> str:
-        _, _, exc_tb = error_detail.exc_info()
+        _, _, exc_tb = traceback.sys.exc_info()  ## this will give us the traceback object
         file_name = exc_tb.tb_frame.f_code.co_filename
         line_number = exc_tb.tb_lineno
         error_message = f"Error occurred in script: {file_name} at line number: {line_number}"
@@ -16,3 +22,4 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.message
+
